cfbe75bf32a8d86720980c4f5d71baf59b020907
Joaquin Orozco <joaquinalexisorozco@gmail.com>
2023-02-12 20:39:51 -0300

    facedetect1.0

--- src/main/java/org/example/FaceDetection.java
+++ src/main/java/org/example/FaceDetection.java
@@ -1,31 +1,77 @@
 package org.example;
 
-import org.opencv.core.*;
-import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.videoio.VideoCapture;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 public class FaceDetection {
-    public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    static { System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
-        CascadeClassifier faceDetector = new CascadeClassifier();
-        faceDetector.load("path/to/haarcascade_frontalface_default.xml");
+    private JFrame frame;
+    private VideoCapture capture;
 
-        Mat image = Imgcodecs.imread("path/to/inputImage.jpg");
-        MatOfRect faceDetections = new MatOfRect();
-        faceDetector.detectMultiScale(image, faceDetections);
+    public FaceDetection() {
+        frame = new JFrame("Face Detection");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(640, 480);
+        frame.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+        frame.setVisible(true);
 
-        System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
-
-        for (Rect rect : faceDetections.toArray()) {
-            Imgproc.rectangle(image, new Point(rect.x, rect.y),
-                    new Point(rect.x + rect.width, rect.y + rect.height),
-                    new Scalar(0, 0, 255), 2);
-        }
-
-        String filename = "path/to/outputImage.jpg";
-        Imgcodecs.imwrite(filename, image);
+        capture = new VideoCapture(0);
+        capture.set(3, 640);
+        capture.set(4, 480);
     }
 
+    public void runMainLoop(String[] args) {
+        CascadeClassifier faceDetector = new CascadeClassifier("C:/Users/joaqu/Downloads/opencv/sources/data/haarcascades_cuda/haarcascade_frontalface_default.xml");
+
+        Mat webcamImage = new Mat();
+        MatOfRect faceDetections = new MatOfRect();
+
+        while (true) {
+            capture.read(webcamImage);
+
+            if (!webcamImage.empty()) {
+                faceDetector.detectMultiScale(webcamImage, faceDetections);
+
+                for (Rect rect : faceDetections.toArray()) {
+                    Imgproc.rectangle(webcamImage, new Point(rect.x, rect.y),
+                            new Point(rect.x + rect.width, rect.y + rect.height),
+                            new Scalar(0, 0, 255), 2);
+                }
+
+                frame.setSize(webcamImage.width() + 40, webcamImage.height() + 60);
+                frame.setVisible(true);
+                FrameFaceDetection display = new FrameFaceDetection(webcamImage);
+                frame.setContentPane(display);
+                frame.validate();
+            } else {
+                System.out.println(" --(!) No captured frame -- Break!");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new FaceDetection().runMainLoop(args));
+    }
+
+    class FrameFaceDetection extends javax.swing.JPanel {
+        private static final long serialVersionUID = 1L;
+        private Mat image;
+
+        public FrameFaceDetection(Mat image) {
+            this.image = image;
+        }
+    }
 }
